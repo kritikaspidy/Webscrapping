a07a594318df5f52f8825fb704a174bca13b0940
Kritika Garg <kritikagarg351@gmail.com>
2025-09-24 21:38:05 +0530

    Deployment frontend 3

--- Frontend/frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: {
-    root: './Frontend/frontend', // or explicitly 'Frontend/frontend' if needed relative path
-  },
-};
-
-module.exports = nextConfig;
