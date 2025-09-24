2f2f2712808ebd901fbb2e7317f5957c0f217745
Kritika Garg <kritikagarg351@gmail.com>
2025-09-25 01:47:25 +0530

    deploy final3

--- Backend/backend/src/main.ts
+++ Backend/backend/src/main.ts
@@ -1,28 +1,19 @@
+// In: Backend/backend/src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './http-exception.filter';
 import { LoggingInterceptor } from './logging.interceptor';
-import 'dotenv/config';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Enable CORS
-  app.enableCors({
-    origin: 'http://localhost:3001',
-  });
-
-  // Apply validation pipe globally
-  app.useGlobalPipes(new ValidationPipe());
-
-  // Apply exception filter globally
   app.useGlobalFilters(new HttpExceptionFilter());
-
-  // Apply logging interceptor globally
   app.useGlobalInterceptors(new LoggingInterceptor());
 
-  await app.listen(process.env.PORT ?? 3000);
+  // --- ADD THIS LINE ---
+  app.enableCors();
+  // --------------------
+
+  await app.listen(3000);
 }
 bootstrap();
